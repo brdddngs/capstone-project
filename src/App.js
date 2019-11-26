4d99ca5e3c3a9ec7e5ac236574a7c0d97c511fe2
Britta Kloth <britta.kl@hotmail.de>
2019-11-26 14:51:59 +0100

    Showrecipes by data-json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react'
+import './styles.scss'
+import recipes from './recipes.json'
 
 function App() {
   return (
     <div className="App">
-      <div>Hola!</div>
+      {recipes.map((recipe, index) => (
+        <div key={index} className="container">
+          <h2>{recipe.title}</h2>
+          <p>{recipe.steps}</p>
+          {recipe.ingredients.map((ingredient, index) => (
+            <div key={index} className="ingredient-wrapper">
+              <p>{ingredient.amount}</p>
+              <p>{ingredient.unit}</p>
+              <p>{ingredient.food}</p>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
