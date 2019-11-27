4eba6abc152b586c06e24621a331982456940110
Britta Kloth <britta.kl@hotmail.de>
2019-11-27 11:32:42 +0100

    Implement plain recipes overview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import './styles.scss'
+import Recipe from './Recipe'
 import recipes from './recipes.json'
 
 function App() {
   return (
     <div className="App">
       {recipes.map((recipe, index) => (
-        <div key={index} className="container">
-          <h2>{recipe.title}</h2>
-          <p>{recipe.steps}</p>
-          {recipe.ingredients.map((ingredient, index) => (
-            <div key={index} className="ingredient-wrapper">
-              <p>{ingredient.amount}</p>
-              <p>{ingredient.unit}</p>
-              <p>{ingredient.food}</p>
-            </div>
-          ))}
-        </div>
+        <Recipe key={index} {...recipe} />
       ))}
     </div>
   )
