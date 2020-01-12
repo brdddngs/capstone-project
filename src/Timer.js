612997ab721f9c1df180d4156a08a388a58f028f
Britta Kloth <britta.kl@hotmail.de>
2020-01-12 15:35:56 +0100

    Add styling and let time show pretty

--- src/Timer.js
+++ src/Timer.js
@@ -3,10 +3,16 @@ import styled from 'styled-components/macro'
 
 export default function Timer({ time }) {
   const [counter, setCounter] = useState(time)
+  const minutes = Math.floor(counter / 60).toString()
+  const seconds = (counter - minutes * 60).toString()
+  console.log(seconds)
 
   return (
     <Container>
-      <Counter>{counter}</Counter>
+      <Time>
+        {minutes < 10 ? `0${minutes}` : minutes}:
+        {seconds < 10 ? `0${seconds}` : seconds}
+      </Time>
     </Container>
   )
 }
@@ -19,7 +25,15 @@ const Container = styled.section`
   width: 80px;
   display: grid;
   justify-content: center;
-  align-items: center;
 `
 
-const Counter = styled.p``
+const Time = styled.p`
+  margin: 0;
+  padding: 2px 14px;
+  background-color: #e29413;
+  border-radius: 12px;
+  box-shadow: 0 0 4px 0 rgba(49, 49, 49, 0.5);
+  font-size: 1rem;
+  font-weight: 800;
+  color: #f2f2f2;
+`
